e25e6d9d9c7b86b2d655348cf9e10a3daf9f7922
Naveenkumar <nawinkumar94@gmail.com>
2020-12-22 22:59:02 +0530

    correction commit

--- nginx.sh
+++ nginx.sh
@@ -4,6 +4,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 
+apt-get install mysql-server
+apt-get install mysql-client
+
 apt-cache policy docker-ce
 apt-get install -y docker-ce
 
@@ -12,6 +15,3 @@ docker pull nginx:latest
 
 # run container with port mapping - host:container
 docker run -d -p 80:80 --name nginx nginx
-
-apt-get install mysql-server
-apt-get install mysql-client
